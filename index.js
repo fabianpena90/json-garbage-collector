4534c277462015552ea9a1d4a4697a3520cd32ec
Fabian <camilo.fabis@gmail.com>
2021-08-10 09:43:10 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,68 @@
+const R = require("ramda");
+const fs = require("fs").promises;
+const globby = require("globby");
+const dotNotationRegex = /("|'|`)((?:\w+[.])+\w+)("|'|`)/g;
+// const dotNotationDynamicRegex = /("|')((?:\w+[.])+(\w|\w*\$\{\w+\})+)("|')/g
+const englishTokens = require("./webapp/public/en-us.json");
+const dir = "../testing-scripts/output";
+(async () => {
+  const options = {
+    gitignore: true,
+  };
+  const cwd = process.cwd();
+  const results = {};
+  for await (const path of globby.stream(
+    [
+      "webapp/src/js/**/*.(js|jsx)",
+      "webapp/src/html/**/*.html",
+      "webapp/src/authApp/**/*.(js|jsx)",
+      "webapp/src/eusApp/**/*.(js|jsx)",
+      "webapp/src/remoteSupportLandingPage/**/*.(js|jsx)",
+      "webapp/src/superAdminApp/**/*.(js|jsx)",
+      "webapp/src/*.js",
+      "webapp/src/reportViewer/**/*.(js|jsx)",
+    ],
+    options
+  )) {
+    if (
+      ["de-de.js", "en-us.js", "es-es.js", "fr-fr.js"].some((i) =>
+        path.endsWith(i)
+      )
+    ) {
+      continue;
+    }
+    const fileName = `${cwd}/${path}`;
+    const fileContent = await fs.readFile(fileName, "utf8");
+    const matches = fileContent.match(dotNotationRegex);
+    if (!matches) continue;
+    for (const match of matches) {
+      const m = match.replace(/"|'/g, "");
+      if (englishTokens[m]) {
+        const item = results[m];
+        if (item) {
+          item.push(path);
+        } else {
+          results[m] = [path];
+        }
+      }
+    }
+  }
+  console.log("allTokens", Object.keys(englishTokens).length);
+  // const allKeys = Object.keys(englishTokens);
+  // console.log("results", Object.keys(results).length);
+  console.log("results", Object.keys(results).length);
+  const resultKeys = R.pick(Object.keys(results), englishTokens);
+  console.log("final", Object.keys(resultKeys).length);
+
+  await fs.mkdir(dir, { recursive: true });
+  fs.writeFile(
+    "../testing-scripts/output/resultKeys.json",
+    JSON.stringify(resultKeys)
+  );
+  fs.writeFile(
+    "../testing-scripts/output/results.json",
+    JSON.stringify(results)
+  );
+  // console.log("resultKeys", resultKeys);
+  // console.log("results", results);
+})();
